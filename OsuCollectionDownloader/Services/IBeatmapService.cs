905ea42d1571ecee901bcf5304f74ce6126cc80b
Wayla <nikosnaoum127@gmail.com>
2023-11-24 20:30:07 +0200

    renamed services interface

--- OsuCollectionDownloader/Services/IBeatmapService.cs
+++ OsuCollectionDownloader/Services/IBeatmapService.cs
@@ -3,10 +3,8 @@ using System.Text.Json;
 
 namespace OsuCollectionDownloader.Services;
 
-internal interface IMirrorService
+internal interface IBeatmapService
 {
-    string BaseApiUrl { get; }
-
     Task<Result<object?>> SearchAsync(string query, CancellationToken token);
 
     Task<Result<bool>> DownloadAsync(string filePath, int beatmapSetId, CancellationToken token);
