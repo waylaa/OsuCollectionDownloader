e31eeb72feeaefb65c1973d9e9faceb8387e32cf
Wayla <nikosnaoum127@gmail.com>
2023-11-24 20:20:09 +0200

    updated access modifiers

--- OsuCollectionDownloader/Logging/DownloadProcessorLogger.cs
+++ OsuCollectionDownloader/Logging/DownloadProcessorLogger.cs
@@ -7,29 +7,29 @@ internal static partial class DownloadProcessorLogger
     [LoggerMessage(EventId = 0, Level = LogLevel.Warning, Message = "The specified directory '{extractionDirectory}' does not point to the 'Songs' folder within your osu! directory. " +
         "Failing to do so prevents the application from checking for existing beatmaps, potentially slowing down the download progress and extracted beatmaps will not be able to be " +
         "saved in the 'Songs' folder.")]
-    public static partial void UnsupportedExtractionDirectory(this ILogger logger, string extractionDirectory);
+    internal static partial void UnsupportedExtractionDirectory(this ILogger logger, string extractionDirectory);
 
     [LoggerMessage(EventId = 1, Level = LogLevel.Information, Message = "Attempting to fetch the metadata and beatmaps from the collection. This may take a while.")]
-    public static partial void OngoingCollectionFetch(this ILogger logger);
+    internal static partial void OngoingCollectionFetch(this ILogger logger);
 
     [LoggerMessage(EventId = 2, Level = LogLevel.Error, Message = "Could not fetch the metadata or beatmaps from the collection.")]
-    public static partial void UnsuccessfulCollectionFetch(this ILogger logger);
+    internal static partial void UnsuccessfulCollectionFetch(this ILogger logger);
 
     [LoggerMessage(EventId = 3, Level = LogLevel.Information, Message = "'{BeatmapName} already exists.'")]
-    public static partial void AlreadyExists(this ILogger logger, string beatmapName);
+    internal static partial void AlreadyExists(this ILogger logger, string beatmapName);
 
     [LoggerMessage(EventId = 5, Level = LogLevel.Error, Message = "'{BeatmapName}' could not be extracted and has been deleted as such.")]
-    public static partial void ExtractionFailure(this ILogger logger, string beatmapName);
+    internal static partial void ExtractionFailure(this ILogger logger, string beatmapName);
 
     [LoggerMessage(EventId = 7, Level = LogLevel.Information, Message = "'{BeatmapName}' has been downloaded successfully.")]
-    public static partial void SuccessfulDownload(this ILogger logger, string beatmapName);
+    internal static partial void SuccessfulDownload(this ILogger logger, string beatmapName);
 
     [LoggerMessage(EventId = 8, Level = LogLevel.Error, Message = "Could not download '{BeatmapName}'.")]
-    public static partial void UnsuccessfulDownload(this ILogger logger, string beatmapName);
+    internal static partial void UnsuccessfulDownload(this ILogger logger, string beatmapName);
 
     [LoggerMessage(EventId = 9, Level = LogLevel.Information, Message = "Download finished.")]
-    public static partial void DownloadFinished(this ILogger logger);
+    internal static partial void DownloadFinished(this ILogger logger);
 
     [LoggerMessage(EventId = 10, Level = LogLevel.Information, Message = "Generated a .osdb collection at {OsdbFilePath}.")]
-    public static partial void OsdbCollectionSuccessfulGeneration(this ILogger logger, string osdbFilePath);
+    internal static partial void OsdbCollectionSuccessfulGeneration(this ILogger logger, string osdbFilePath);
 }
