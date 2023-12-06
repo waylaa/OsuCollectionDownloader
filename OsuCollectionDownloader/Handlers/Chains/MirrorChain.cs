879b5508e3036e1303da74d97b893d44089298cc
Wayla <nikosnaoum127@gmail.com>
2023-12-06 23:18:29 +0200

    changed access modifiers and other things

--- OsuCollectionDownloader/Handlers/Chains/MirrorChain.cs
+++ OsuCollectionDownloader/Handlers/Chains/MirrorChain.cs
@@ -5,23 +5,23 @@ namespace OsuCollectionDownloader.Handlers.Chains;
 
 internal sealed class MirrorChain : IEnumerable<IBeatmapMirrorHandler>
 {
-    private ISet<IBeatmapMirrorHandler> Mirrors { get; } = new HashSet<IBeatmapMirrorHandler>();
+    private readonly HashSet<IBeatmapMirrorHandler> _mirrors = [];
 
     public IEnumerator<IBeatmapMirrorHandler> GetEnumerator()
-        => Mirrors.GetEnumerator();
+        => _mirrors.GetEnumerator();
 
-    public async Task<Result<bool>> HandleAsync(string title, string difficultyName, string filePath, CancellationToken token)
+    internal async Task<Result<bool>> HandleAsync(string title, string difficultyName, string filePath, CancellationToken token)
     {
-        if (Mirrors.Count == 0)
+        if (_mirrors.Count == 0)
         {
             return false;
         }
 
-        foreach (IBeatmapMirrorHandler handler in Mirrors)
+        foreach (IBeatmapMirrorHandler handler in _mirrors)
         {
             Result<bool> result = await handler.HandleAsync(title, difficultyName, filePath, token);
 
-            if (!result.IsSuccessful || !result.Value)
+            if (!result.IsSucessfulWithValue || !result.Value)
             {
                 continue;
             }
@@ -34,10 +34,10 @@ internal sealed class MirrorChain : IEnumerable<IBeatmapMirrorHandler>
 
     internal MirrorChain Add(IBeatmapMirrorHandler handler)
     {
-        Mirrors.Add(handler);
+        _mirrors.Add(handler);
         return this;
     }
 
     IEnumerator IEnumerable.GetEnumerator()
-        => ((IEnumerable)Mirrors).GetEnumerator();
+        => _mirrors.GetEnumerator();
 }
