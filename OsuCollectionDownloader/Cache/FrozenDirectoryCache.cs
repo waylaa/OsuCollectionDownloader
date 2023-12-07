c4568bf6ca88a4e8ef4c37a1c191c042c6006c74
Wayla <nikosnaoum127@gmail.com>
2023-12-07 20:31:11 +0200

    added an in-memory cache for searching for existing beatmapsets instead of looping every time the extraction directory which is slow.

---
+++ OsuCollectionDownloader/Cache/FrozenDirectoryCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Frozen;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsuCollectionDownloader.Cache;
+
+internal sealed class FrozenDirectoryCache(string path)
+{
+    internal FrozenSet<string> Items { get; } = new HashSet<string>(Directory.EnumerateDirectories(path), StringComparer.OrdinalIgnoreCase).ToFrozenSet();
+}
