5150f92e9505a87e249f181cdd13e40af5a03099
Wayla <nikosnaoum127@gmail.com>
2023-12-29 18:31:38 +0200

    moved interface

---
+++ OsuCollectionDownloader/Handlers/Abstractions/IMirrorHandler.cs
@@ -0,0 +1,8 @@
+﻿using OsuCollectionDownloader.Objects;
+
+namespace OsuCollectionDownloader.Handlers.Abstractions;
+
+internal interface IMirrorHandler
+{
+    Task<Result<bool>> DownloadAsync(string filePath, int beatmapSetId, CancellationToken token);
+}
